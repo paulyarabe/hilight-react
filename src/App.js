117a0ffcc189752e0039f450c20240ae6eb2c1f3
github username <github email address>
2017-11-03 12:00:51 -0400

    removed comments feature

--- src/App.js
+++ src/App.js
@@ -8,45 +8,16 @@ class App extends Component {
     super()
     this.state = {
       hilight: [],
-      display: '',
-      comments: [],
-      jwt: '',
-      isLoggedIn: false,
-      user: {}
+      display: ''
     }
   }
-
-  // login(loginParams){
-  //   Auth.login(loginParams)
-  //   .then((json)=>{
-  //     localStorage.setItem('jwt', json.jwt)
-  //     this.setState({
-  //       jwt: json.jwt,
-  //       isLoggedIn: true
-  //     })
-  //   })
-  // }
-
-  // currentUser(){
-  //   Auth.currentUser()
-  //   .then((user) => {
-  //     console.log(user)
-  //     this.setState({ user } )
-  //   })
-  // }
-  //
-  // //where you do all API calls.
-  // componentDidMount(){
-  //   this.currentUser()
-  // }
 
   onMouseEnter = () => {
     fetch(`https://hilight-db.herokuapp.com/hilight`)
     .then(resp => resp.json())
     .then(hilight_data => this.setState({
       hilight: hilight_data,
-      display: 'HiLight / No. ',
-      comments: hilight_data.comments
+      display: 'HiLight / No. '
      }))
   }
 
@@ -60,8 +31,6 @@ class App extends Component {
       <h2><span>{this.state.display + this.state.hilight.id}</span></h2>
       <p>{this.state.hilight.highlighted_text}</p>
       <h6>- <u><a id="booklink" href={this.state.hilight.url} target="_blank">{this.state.hilight.book_title}</a></u></h6></div> : null
-
-    // let comment = this.state.hilight.comments.map((comment) => <h4>{comment.comment}</h4>)
 
     return (
       <div className="App">
@@ -78,14 +47,6 @@ class App extends Component {
         </div>
         <div id="hilight" onMouseEnter={this.onMouseEnter} >
           {highlight_div}
-        </div>
-        {/*
-          <div id="sayHi">
-            Say Hi here!
-          </div>
-        */}
-        <div>
-              {this.state.comments.map((comment, id) => <div className="dialogbox"><div className="body"><span className="tip tip-left"></span><div className="message"><span>{comment.comment}<br /> <span style={{float:'right', marginTop: '-10px'}}>- {comment.commentator}</span></span></div></div></div>)}
         </div>
           <div className="footer">
             <div className="container">
